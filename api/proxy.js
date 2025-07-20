b993b05606e7e99c3ff027097e9074ffe0c49c75
Lucas-Emanoel <lucasemanoel5579@icloud.com>
2025-07-20 12:36:51 -0400

    proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -25,7 +25,7 @@ module.exports = async (req, res) => {
     const videoResponse = await fetch(videoUrl, {
       headers: {
         'Range': req.headers.range || '',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
       }
     });
 
